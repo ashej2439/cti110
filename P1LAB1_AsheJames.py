38d3b1c1e595828655991e1d72a1028648dcf6bc
ashej2439 <ashej2439@student.faytechcc.edu>
2024-09-22 11:01:29 -0400

    Add files via upload
    First submission

---
+++ P1LAB1_AsheJames.py
@@ -0,0 +1,8 @@
+#James Ashe
+#09/21/2024
+#CTI-P1LAB1
+#Python program using IDLE showing how to get input from the user and displays output.
+first_name = input("Enter your first name:  ") 
+last_name = input("Enter your last name:  ") 
+print('Hello,', first_name, last_name + '! Welcome to CTI-110')
+
